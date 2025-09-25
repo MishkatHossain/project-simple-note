26f1210e5f334448af86946104e368dc61d3c9d4
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-09-25 20:35:05 +0600

    max

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div>
+        Mishkat This is Forbidden 403
+    </div>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+
+
+
+
+
+Route::redirect('/', '/note')->name('dashboard');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('note', NoteController::class);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
